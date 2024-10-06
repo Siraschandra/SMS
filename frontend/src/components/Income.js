aec7b8b4629139a508f95acad97c2c822109be6d
Siraschandra <71237994+Siraschandra@users.noreply.github.com>
2024-10-06 14:51:31 +0530

    Update Income.js

--- frontend/src/components/Income.js
+++ frontend/src/components/Income.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FormatDate from "./FormatDate";
 
 export default class Income extends Component {
   render() {
@@ -6,7 +7,7 @@ export default class Income extends Component {
     return (
       <div className="innerIncome">
         <p>{this.props.i}</p>
-        <p>{new Date(this.props.createdAt).format("YYYY-MM-DD")}</p>
+        <p>{FormatDate(this.props.createdAt)}</p>
         <p>
           Total Fees collected Rs{" "}
           {this.props.monthly_fees +
